48c6717d4a22337e62fbb8308bb954d915460424
Owen Levin <Owenlevin@gmail.com>
2016-12-12 10:56:33 -0800

    circle nav

--- js/hamburger.js
+++ js/hamburger.js
@@ -19,34 +19,66 @@ $("#hamburger").on("click", animation);
 $(document).ready(function(){
     $(window).scroll(function(){
         if ($(window).scrollTop() > 550){
-          $(".line").css("background-color", "black");
-          $("nav ul").css("color", "white");
-          $("nav ul").css("background-color", "rgba(0, 0, 0, .6)");
+          // $(".line").css("background-color", "black");
+          // $("nav ul").css("color", "white");
+          // $("nav ul").css("background-color", "rgba(0, 0, 0, .6)");
+          $(".media ul li a").css("color","black");
         }else{
-          $("nav ul").css("background-color", "rgba(255, 255, 255, .6)");
-          $("nav ul").css("color", "black");
-          $(".line").css("background-color", "white");
-
+          // $("nav ul").css("background-color", "rgba(255, 255, 255, .6)");
+          // $("nav ul").css("color", "black");
+          // $(".line").css("background-color", "white");
+          $(".media ul li a").css("color","white");
         }
     });
 });
 
 
 function toggleNav(){
-  if(nav.hasClass("opened")){
-    $("nav ul").animate({
-      left: "-=13vw"
-    });
-    setTimeout(function() {
-      nav.css("display", "none");
-    }, 350);
-    $("nav ul").css("position", "fixed");
-    nav.removeClass("opened");
+
+  if($(".media").hasClass("opened")){
+      $(".media").removeClass("opened");
+
+      $(".media ul li:first-child").animate({
+          left: "-=6vw"
+      });
+      $(".media ul li:nth-child(2)").animate({
+          left: "-=4.5vw",top:"-=4.5vw"
+      });
+      $(".media ul li:last-child").animate({
+          top: "-=7vw"
+      });
+
+
   }else{
-    $("nav ul").animate({
-      left: "+=13vw"
+    $(".media ul li").css("display","block");
+    $(".media ul li").css("translate","rotate(45deg)");
+    $(".media ul li:first-child").animate({
+        left: "+=6vw"
     });
-    nav.css("display", 'block');
-    nav.addClass("opened");
+    $(".media ul li:nth-child(2)").animate({
+        left: "+=4.5vw",top:"+=4.5vw"
+    });
+    $(".media ul li:last-child").animate({
+        top: "+=7vw"
+    });
+
+    $(".media").addClass("opened");
   }
+
+  // if(nav.hasClass("opened")){
+  //   $("nav ul").animate({
+  //     left: "-=9vw"
+  //   });
+  //   setTimeout(function() {
+  //     nav.css("display", "none");
+  //   }, 350);
+  //   $("nav ul").css("position", "fixed");
+  //   nav.removeClass("opened");
+  // }else{
+  //   $("nav ul").animate({
+  //     left: "+=9vw"
+  //   });
+  //   nav.css("display", 'block');
+  //   nav.addClass("opened");
+  // }
 }
